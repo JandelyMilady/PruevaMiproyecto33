6776986f8efc55b529b6a0bd03e58fcd2323dc3b
Alumno <alumno@localhost.localdomain>
2014-01-29 08:30:42 -0600

    Subiendo un proyecto

---
+++ src/Miproyecto33.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class Miproyecto33 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Esto es para q lo muestre en la consola
+System.out.println("Hola");
+//Esto lee la consola mi_var... es una varible
+Scanner mi_scanner=new Scanner(System.in);
+int mi_variable = mi_scanner.nextInt();
+System.out.println(mi_variable*2);
+if (mi_variable==10)
+{
+	System.out.println("Ingresaste un 10");
+}
+System.out.println("fin del Programa");
+	}
+
+}
